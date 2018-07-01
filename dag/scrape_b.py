bbc3b03dda76bdbe13327df6329d835b8abfc3a9
Jake Park <danbi2990@gmail.com>
2018-07-01 20:19:57 +0900

    Test

--- dag/scrape_b.py
+++ dag/scrape_b.py
@@ -3,9 +3,6 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 
-ASSEMBLY_PYTHON = '$HOME/.virtualenvs/assembly/bin/python3.6'
-ASSEMBLY_PROJECT = '$HOME/Dev/assembly'
-
 default_args = {
     'start_date': datetime(2018, 7, 1),
     'retries': 2,
@@ -20,7 +17,7 @@ dag = DAG(
     default_args=default_args,
 )
 
-cmd0 = f'{ASSEMBLY_PYTHON} {ASSEMBLY_PROJECT}/scrape/a0_watch_committee.py'
+cmd0 = 'touch /home/jake/test.txt'
 
-task0 = BashOperator(task_id='a0_watch_committee',
+task0 = BashOperator(task_id='b0_touch_file',
                      bash_command=cmd0, dag=dag)
